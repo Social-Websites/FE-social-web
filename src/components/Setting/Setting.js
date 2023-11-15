93983db9b5e21a3b5f46432c18cc1ecebb13af83
Duong <ngkhacduong@gmail.com>
2023-11-15 20:40:03 +0700

    ui + rebound search

---
+++ src/components/Setting/Setting.js
@@ -0,0 +1,22 @@
+import classNames from 'classnames/bind';
+import { React } from "react";
+import styles from "./Setting.module.scss";
+
+
+const cx = classNames.bind(styles);
+function Setting() {
+    return (
+        <div className={cx("settings")} >
+            {/* {notifications.map((n) => displayNotification(n))} */}
+            <div className={cx("settings__title")}>
+                <span >Settings</span>
+            </div>
+            <div className={cx("settings__content")} >
+                <div className={cx("settings__content__element")}>Edit profile</div>
+                <div className={cx("settings__content__element")}>Push notifications</div>
+                <div className={cx("settings__content__element")}>What you see</div>
+            </div>
+        </div>
+    )
+}
+export default Setting;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,4 @@
+import * as httprequest from '../shared/util/httprequest';
 export const getUser = async (sendRequest) => {
   try {
     const response = await sendRequest("/users/auth-user");
