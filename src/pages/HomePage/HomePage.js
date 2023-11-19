d5957b79427477015352f7e821a548d871e03fe8
Duong <ngkhacduong@gmail.com>
2023-11-19 19:51:48 +0700

    fix background

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import classNames from "classnames/bind";
-import styles from "./HomePage.scss";
+import styles from "./HomePage.module.scss";
 import Sidenav from "../../shared/components/NavBar";
 import Timeline from "../../components/TimeLine";
 import useLogout from "../../shared/hook/auth-hook/logout-hook";
